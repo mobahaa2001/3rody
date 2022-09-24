4c1a3e9bd36016e62789e4559db8a0e4ca37e2b4
Guide <70282836+mobahaa2001@users.noreply.github.com>
2022-09-24 06:53:06 +0200

    Update HomePage.js

--- src/Pages/Home/HomePage.js
+++ src/Pages/Home/HomePage.js
@@ -1,23 +1,19 @@
 import React from 'react'
-import NavBarLogin from '../../Components/UItily/NavBarLogin'
 import Slider from '../../Components/Home/Slider'
 import HomeCategory from '../../Components/Home/HomeCategory'
 import CardProductContainer from '../../Components/Products/CardProductContainer'
 import DiscountSection from '../../Components/Home/DiscountSection'
 import BrandFeatured from '../../Components/Brand/BrandFeatured'
-import Footer from '../../Components/UItily/Footer'
 
 const HomePage = () => {
   return (
     <div className="font" style={{ minHieght: '670px' }}>
-      <NavBarLogin />
       <Slider />
       <HomeCategory />
       <CardProductContainer title="Best Sellers" btntitle="SHOP NOW" />
       <DiscountSection />
       <CardProductContainer title="Up to 70% Off" btntitle="SHOP NOW" />
       <BrandFeatured />
-      <Footer />
     </div>
   )
 }
