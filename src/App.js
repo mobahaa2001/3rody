58a4aaaa52f9feb567c5b593308557c509597c1d
Guide <70282836+mobahaa2001@users.noreply.github.com>
2022-09-24 06:52:19 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import HomePage from './Pages/Home/HomePage'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import NavBarLogin from './Components/UItily/NavBarLogin'
+import Footer from './Components/UItily/Footer'
+import LoginPage from './Pages/Auth/LoginPage'
 
 function App() {
   return (
-    <div>
-      <HomePage />
+    <div className="font">
+      <NavBarLogin />
+
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<HomePage />} />
+          <Route path='/login' element={<LoginPage />} />
+
+        </Routes>
+      </BrowserRouter>
+      <Footer />
     </div>
   )
 }
