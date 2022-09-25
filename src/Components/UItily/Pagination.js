9b189de8c9372e9c7b18c04865e350e56832c0a3
Guide <70282836+mobahaa2001@users.noreply.github.com>
2022-09-25 15:14:09 +0200

    Add files via upload

---
+++ src/Components/UItily/Pagination.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactPaginate from "react-paginate";
+
+ const Pagination = () => {
+     const handlePageClick = () => { };
+  return (
+
+
+        <ReactPaginate
+            breakLabel="..."
+            nextLabel="Next"
+            onPageChange={handlePageClick}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={2}
+            pageCount={100}
+            previousLabel="Back"
+            containerClassName={"pagination justify-content-center p-3"}
+            pageClassName={"page-item"}
+            pageLinkClassName={"page-link"}
+            previousClassName={"page-item"}
+            nextClassName={"page-item"}
+            previousLinkClassName={"page-link"}
+            nextLinkClassName={"page-link"}
+            breakClassName={"page-item"}
+            breakLinkClassName={"page-link"}
+            activeClassName={"active"}
+        />
+    )
+}
+
+
+export default Pagination
