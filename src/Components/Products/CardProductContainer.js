8bc9245174443cbe1f9f8ca7d79d0a08ef1b1203
Guide <70282836+mobahaa2001@users.noreply.github.com>
2022-09-26 14:45:07 +0200

    Update CardProductContainer.js

--- src/Components/Products/CardProductContainer.js
+++ src/Components/Products/CardProductContainer.js
@@ -3,10 +3,10 @@ import { Container, Row } from 'react-bootstrap'
 import SubTiltle from '../UItily/SubTitle'
 import ProductCard from './ProductCard'
 
-const CardProductContainer = ({ title, btntitle }) => {
+const CardProductContainer = ({ title, btntitle, pathText }) => {
   return (
     <Container>
-      <SubTiltle title={title} btntitle={btntitle} />
+      <SubTiltle title={title} btntitle={btntitle} pathText={pathText} />
       <Row className="my-2 d-flex justify-content-between">
         <ProductCard />
         <ProductCard />
