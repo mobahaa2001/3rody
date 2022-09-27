422b02e97336de0acd08e206acc8e8f86f41d386
Guide <70282836+mobahaa2001@users.noreply.github.com>
2022-09-27 20:00:13 +0200

    Update ProductDetalisPage.js

--- src/Pages/Products/ProductDetalisPage.js
+++ src/Pages/Products/ProductDetalisPage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
 import CategoryHeader from '../../Components/Category/CategoryHeader'
+import CardProductContainer from '../../Components/Products/CardProductContainer'
 import ProductDetalis from '../../Components/Products/ProductDetails'
 import RateContainer from '../../Components/Rate/RateContainer'
 
@@ -11,6 +12,7 @@ const ProductDetailsPage = () => {
       <Container>
         <ProductDetalis />
         <RateContainer />
+        <CardProductContainer title="Customers who viewed items in your browsing history also viewed" />
       </Container>
     </div>
   )
