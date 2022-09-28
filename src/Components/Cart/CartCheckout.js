ccb1db362864838bff487013851208d52d507dbe
Guide <70282836+mobahaa2001@users.noreply.github.com>
2022-09-28 11:40:44 +0200

    Update CartCheckout.js

--- src/Components/Cart/CartCheckout.js
+++ src/Components/Cart/CartCheckout.js
@@ -3,26 +3,29 @@ import { Row, Col } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 const CartCheckout = () => {
-    return (
-        <Row className="my-1 d-flex justify-content-center cart-checkout pt-3">
-            <Col xs="12" className="d-flex  flex-column  ">
-                <div className="d-flex  ">
-                    <input
-                        className="copon-input d-inline text-center "
-                        placeholder="كود الخصم"
-                    />
-                    <button className="copon-btn d-inline ">تطبيق</button>
-                </div>
-                <div className="product-price d-inline w-100 my-3  border">34000 جنية</div>
-                <Link
-                    to="/order/paymethoud"
-                    style={{ textDecoration: "none" }}
-                    className="product-cart-add  d-inline ">
-                    <button className="product-cart-add w-100 px-2"> اتمام الشراء</button>
-                </Link>
-            </Col>
-        </Row>
-    )
+  return (
+    <Row className="my-1 d-flex justify-content-center cart-checkout pt-3">
+      <Col xs="12" className="d-flex  flex-column  ">
+        <div className="d-flex  ">
+          <input
+            className="copon-input d-inline text-center "
+            placeholder="Enter Coupon"
+          />
+          <button className="copon-btn d-inline ">Apply</button>
+        </div>
+        <div className="product-price d-inline w-100 my-3  border">
+          EGP 30,770.00
+        </div>
+        <Link
+          to="/order/paymethoud"
+          style={{ textDecoration: 'none' }}
+          className="product-cart-add  d-inline "
+        >
+          <button className="product-cart-add w-100 px-2">Checkout</button>
+        </Link>
+      </Col>
+    </Row>
+  )
 }
 
 export default CartCheckout
