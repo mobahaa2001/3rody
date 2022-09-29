dd6a73e8b0d84eae8a6ae670285a6bd0637059ab
Guide <70282836+mobahaa2001@users.noreply.github.com>
2022-09-29 17:42:34 +0200

    Update AdminAllProductPage.js

--- src/Pages/Admin/AdminAllProductPage.js
+++ src/Pages/Admin/AdminAllProductPage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
 import AdminAllProduct from '../../Components/Admin/AdminAllProduct'
 import AdminSideBar from '../../Components/Admin/AdminSideBar'
+import Pagination from '../../Components/UItily/Pagination'
 
 const AdminAllProductPage = () => {
   return (
@@ -10,8 +11,9 @@ const AdminAllProductPage = () => {
         <Col sm="3" xs="2" md="2">
           <AdminSideBar />
         </Col>
-              <Col sm="9" xs="10" md="10">
-                  <AdminAllProduct />
+        <Col sm="9" xs="10" md="10">
+          <AdminAllProduct />
+          <Pagination />
         </Col>
       </Row>
     </Container>
