09d18256de745b632c0bf684c1b292b5776bc7af
Guide <70282836+mobahaa2001@users.noreply.github.com>
2022-09-29 17:43:16 +0200

    Update AdminAllProduct.js

--- src/Components/Admin/AdminAllProduct.js
+++ src/Components/Admin/AdminAllProduct.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import { Row } from 'react-bootstrap'
+import AdminAllProductCard from './AdminAllProductCard'
 
 const AdminAllProduct = () => {
-  return <div>AdminAllProduct</div>
+  return (
+    <div>
+      <div className="admin-content-text">Manage All Products</div>
+      <Row className="justify-content-start">
+        <AdminAllProductCard />
+        <AdminAllProductCard />
+        <AdminAllProductCard />
+        <AdminAllProductCard />
+        <AdminAllProductCard />
+        <AdminAllProductCard />
+        <AdminAllProductCard />
+      </Row>
+    </div>
+  )
 }
 
 export default AdminAllProduct
