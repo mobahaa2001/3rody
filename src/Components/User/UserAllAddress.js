8d2f025e0b68b00ef4810af5ad07d70b4f11452e
Guide <70282836+mobahaa2001@users.noreply.github.com>
2022-10-01 02:30:53 +0200

    Update UserAllAddress.js

--- src/Components/User/UserAllAddress.js
+++ src/Components/User/UserAllAddress.js
@@ -1,25 +1,21 @@
-import React from 'react'
 import { Row, Col } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import UserAddressCard from './UserAddressCard'
 
-const UserAllAddress = () => {
-    return (
-        <div>
-            <div className="admin-content-text pb-4">دفتر العنوانين</div>
-            <UserAddressCard />
-            <UserAddressCard />
-            <UserAddressCard />
+const UserAllAddresses = () => {
+  return (
+    <div>
+      <div className="admin-content-text pb-4">Address Book</div>
+      <UserAddressCard />
 
-            <Row className="justify-content-center">
-                <Col sm="5" className="d-flex justify-content-center">
-                    <Link to="/user/add-address" style={{ textDecoration: "none" }}>
-                        <button className="btn-add-address">اضافه عنوان جديد</button>
-                    </Link>
-                </Col>
-            </Row>
-        </div>
-    )
+      <Row className="justify-content-center">
+        <Col sm="5" className="d-flex justify-content-center">
+          <Link to="/user/add-address" style={{ textDecoration: 'none' }}>
+            <button className="btn-add-address mx-2">Add a new title</button>
+          </Link>
+        </Col>
+      </Row>
+    </div>
+  )
 }
-
-export default UserAllAddress
+export default UserAllAddresses
