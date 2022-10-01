3fd9ea8e55bc04a4d4867cd4bc8aea7ed99ea7a7
Guide <70282836+mobahaa2001@users.noreply.github.com>
2022-10-01 02:04:15 +0200

    Update UserSideBar.js

--- src/Components/User/UserSideBar.js
+++ src/Components/User/UserSideBar.js
@@ -5,23 +5,23 @@ const UserSideBar = () => {
   return (
     <div className="sidebar">
       <div className="d-flex flex-column">
-        <Link to="/admin/allorders" style={{ textDecoration: 'none' }}>
+        <Link to="/user/allorders" style={{ textDecoration: 'none' }}>
           <div className="admin-side-text mt-3 border-bottom p-2 mx-auto text-center">
             Requisition Management{' '}
           </div>
         </Link>
-        <Link to="/admin/allproducts" style={{ textDecoration: 'none' }}>
+        <Link to="/user/favoriteproduct" style={{ textDecoration: 'none' }}>
           <div className="admin-side-text my-1 border-bottom p-2 mx-auto text-center">
             Favorite Products{' '}
           </div>
         </Link>
-        <Link to="/admin/addbrand" style={{ textDecoration: 'none' }}>
+        <Link to="/user/allorders" style={{ textDecoration: 'none' }}>
           <div className="admin-side-text my-1 border-bottom p-2 mx-auto text-center">
             Personal Idiomatic{' '}
           </div>
         </Link>
 
-        <Link to="/admin/addcategory" style={{ textDecoration: 'none' }}>
+        <Link to="/user/allorders" style={{ textDecoration: 'none' }}>
           <div className="admin-side-text my-1 border-bottom p-2 mx-auto text-center">
             Profile{' '}
           </div>
